8bc73531280fe8eaea0ae8f085ee2e2c176481a9
Eftakhairul Islam <eftakhairul@gmail.com>
2013-11-18 00:33:06 -0500

    task 1 and task 2 almost done

--- main.lisp
+++ main.lisp
@@ -1,5 +1,4 @@
 ;; 2013-11-16 02:16:11
-;; This is your lisp file. May it serve you well.
 ;; author Eftakhairul Islam <eftakhairul@gmail.com>
 
 ;;class
@@ -17,17 +16,19 @@
 
 ;;Returns a collection with all keys stored in the dictionary, including possible duplicates.
 (defmethod keys((d dictionary))
- (append (getAllkeys(dictionary-elements d))))
-
+  (getAllkeys(dictionary-elements d))
+)
 
 ;;Returns a collection with all keys stored in the dictionary, including possible duplicates.
 (defmethod elements((d dictionary))
- (append(getAllElements(dictionary-elements d))))
+ (getAllElements(dictionary-elements d))
+)
 
 ;;Inserts an item with element e and key k into the dictionary
 (defmethod insertItem(key element (d dictionary))
   (setf (dictionary-elements d) (cons (list key element) (dictionary-elements d)))
   (setf (dictionary-size d) (+ 1 (dictionary-size d)))
+  T
 )
 
 ;;Returns the number of items in the dictionary.
@@ -44,25 +45,42 @@
 
 ;;Returns an item associated with key, OR False if an item does not exist.
 (defmethod ﬁndItem(key (d dictionary))
-  (searchByKey key (dictionary-elements d))
-  )
+  (if(memberp key (keys d))
+      (searchByKey key (dictionary-elements d))
+      Nil)  
+)
 
 ;;Returns a collection with all items by key.
 (defmethod ﬁndAll(key (d dictionary))
-  (searchAllByKey key (dictionary-elements d))
+  (if(memberp key (keys d))
+      (searchAllByKey key (dictionary-elements d))
+      Nil) 
  )
 
 ;;Removes an item with key equal to k and returns True, or if an item does not exist then it returns False
 (defmethod removeItem(key (d dictionary))
- (setf (dictionary-elements d)(removeOneItemBykey key (dictionary-elements d))))
-
+  (cond ((isEmpty d) nil)
+         ((memberp key (keys d)) (setf (dictionary-elements d)(removeOneItemBykey key (dictionary-elements d)))
+          (setf (dictionary-size d) (- (dictionary-size d) 1))
+          T
+         )             
+    )
+  )
 
 ;;Removes every item with key equal to k and returns True, or if no items exist then it returns False
 (defmethod removeAllItems(key (d dictionary))
- (setf (dictionary-elements d)(removeAllBykey (dictionary-elements d)))
+  (cond ((isEmpty d) nil)
+         ((memberp key (keys d)) (setf (dictionary-elements d)(removeAllBykey key (dictionary-elements d)))
+          (setf (dictionary-size d) (length (dictionary-elements d)))
+          T
+          )             
+    )
 )
 
 
+
+
+;;Inheritance
 ;;Sub Class of Dictonary
 (defclass restricted-dictionary(dictionary)
   ((capacity :accessor restricted-dictionary-capacity             
@@ -73,12 +91,12 @@
 
 ;;Inserts an item with element e and key k into the dictionary
 (defmethod insertItem :around(key element (rd restricted-dictionary))
-  (cond ((equal (stringp key) nil) "error: key is not string")
-        ((equal (stringp element) nil) "error: element is not string")
-        ((equal key element) "error: key and element are same")
-        ((memberp key (keys rd)) "error: key already exist")
+  (cond ((equal (stringp key) nil)      "error: key is not string")
+        ((equal (stringp element) nil)  "error: element is not string")
+        ((equal key element)            "error: key and element are same")
+        ((memberp key (keys rd))        "error: key is already exist")
         ((> (dictionary-size rd) (restricted-dictionary-capacity rd)) "error: you don't have capacity")
-        (T (call-next-method))
+        (T (call-next-method key element rd))
     )  
  )
 
@@ -91,43 +109,69 @@
  )
 
 
+;;======================================== Auxilary functions ======================================
+
+;;return the list after removing the item by key
 (defun removeOneItemBykey(key lst)
  (cond ((null lst) nil)
        ((equal key(car(car lst)))(cdr lst)) 
        (T (cons (car lst) (removeOneItemBykey key (cdr lst)))))
  )
 
+;;return the list after removing all items by key
 (defun removeAllBykey(key lst)
  (cond ((null lst) nil)
-       ((equal key(car(car lst))) (removeAllBykey (cdr lst)))
+       ((equal key(car(car lst))) (removeAllBykey key (cdr lst)))
        (t (cons (car lst) (removeAllBykey key (cdr lst)))))
 )
 
+;;return the element by key from the list
 (defun searchByKey(key lst)
  (cond ((null lst)   nil)
        ((equal (car(car lst)) key) (append(cdr(car lst))))
        (T (searchByKey key (cdr lst))))
 )
 
+;;return all elements by key from the list
 (defun searchAllByKey(key lst)
  (cond ((null lst) nil)
        ((equal (car(car lst)) key) (append (cdr(car lst))(searchAllByKey key (cdr lst))))
        (T (searchAllByKey key (cdr lst))))
 )
 
+;;return all keys
 (defun getAllkeys(lst)
  (if (or (null lst)(not(listp lst))) 
       nil
       (append (cons (car(car lst)) (getAllkeys (cdr lst)))))
 ) 
 
+
+;;return all elements
 (defun getAllElements(lst)
  (if (or (null lst)(not(listp lst))) 
       nil
      (append (cdr(car lst)) (getAllElements (cdr lst))))
  )
-
 ;;================================== Testing Code==============================
+
+;;Check task 1
+(setq d ( make-instance 'dictionary))
+(print (insertItem "a" "Newyork"  d))
+(print (insertItem "b" "Montreal"  d))
+(print (keys  d))
+
+(print (removeItem "b" d))
+(print (size d))
+(print (insertItem "b" "Montreal"  d))
+(print (insertItem "b" "Qubec City"  d))
+(print (insertItem "c" "Toronto"  d))
+(print (display d))
+(print (size d))
+(print (removeAllItems "b" d))
+(print (size d))
+
+
 
 ;;Checking task 2
 (setq rd ( make-instance 'restricted-dictionary))
@@ -147,9 +191,11 @@
 (print (isEmpty rd))
 (write-line "Search by key C")
 (print (ﬁndItem "c"  rd))
-(write-line "Remove one element by key C")
-(print (removeOneItemBykey "c"  rd))
+(print (display rd))
 (write-line "Size")
 (print (size rd))
-
+(write-line "Remove one element by key C")
+(print (removeItem "c" rd))
+(write-line "Size")
+(print (size rd))
 
